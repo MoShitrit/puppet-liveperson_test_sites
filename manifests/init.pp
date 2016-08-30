31b3bbab6703a29450494680a425618d652aefb1
mosheshi <mosheshi@liveperson.com>
2016-08-30 15:32:38 -0400

    Added conf files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class liveperson_test_sites {
-  file { '/usr/share/nginx/html':
+  file { '/liveperson/code/server_openresty/nginx/html':
     ensure => 'directory',
     owner => 'root',
     group => 'root',
@@ -7,7 +7,31 @@ class liveperson_test_sites {
     purge => true,
     force => true,
     mode => 0755,
-    source => 'puppet:///modules/liveperson_test_sites/',
+    source => 'puppet:///modules/liveperson_test_sites/html/',
+  }
+
+  file { '/liveperson/data/server_openresty/logs':
+    ensure => 'directory',
+    owner => 'root',
+    group => 'root',
+    recurse => true,
+    mode => 0755,
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    force => true,
+    source => 'puppet:///modules/liveperson_test_sites/conf/nginx.conf'
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    force => true,
+    source => 'puppet:///modules/liveperson_test_sites/conf/conf.d/default.conf'
   }
 
   package { 'nginx':
